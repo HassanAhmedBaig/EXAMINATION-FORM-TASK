fc3eecfc3435ed5d37cffa7f3e309ddd1e579a5f
HassanAhmedBaig <76657818+HassanAhmedBaig@users.noreply.github.com>
2021-12-25 12:04:19 +0500

    Add files via upload

---
+++ EXAMINATION FORM/app.js
@@ -0,0 +1,77 @@
+function getvalue(){
+    var a = document.getElementById("roll")
+    var b = document.getElementById("rg")
+    var c = document.getElementById("name")
+    var d = document.getElementById("faname")
+    var e = document.getElementById("date")
+    var f = document.getElementById("nat")
+    var g = document.getElementById("mf")
+    var h = document.getElementById("reg")
+    var i = document.getElementById("add")
+    var j = document.getElementById("cn")
+    var k = document.getElementById("con")
+    var l = document.getElementById("mar")
+    var m = document.getElementById("pos")
+    var n = document.getElementById("ur")
+    var o = document.getElementById("ud")
+    var p = document.getElementById("um")
+    var q = document.getElementById("uc")
+    var r = document.getElementById("ug")
+    var s = document.getElementById("ud")
+    var t = document.getElementById("uw")
+    var u = document.getElementById("ux")
+    var v = document.getElementById("uy")
+    var w = document.getElementById("uz")
+    
+    var ab = a.value;
+    var ac = b.value;
+    var ad = c.value;
+    var ae = d.value;
+    var af = e.value;
+    var ag = f.value;
+    var ah = g.value;
+    var ai = h.value;
+    var aj = i.value;
+    var ak = j.value;
+    var al = k.value;
+    var am = l.value;
+    var an = m.value;
+    var ao = n.value;
+    var ap = o.value;
+    var aq = p.value;
+    var ar = q.value;
+    var as = r.value;
+    
+    var at = t.value;
+    var au = u.value;
+    var av = v.value;
+    var aw = w.value;
+
+    
+    document.write("<pre>")
+    document.writeln("ROLL NO :",ab )
+    document.writeln("REGISTRATION NO. :",ac )
+    document.writeln("NAME :",ad )
+    document.writeln("FATHER NAME :",ae )
+    document.writeln("DATE OF BIRTH :",af )
+    document.writeln("NATIONALITY :",ag )
+    document.writeln("MALE/FEMALE :",ah )
+    document.writeln("RELIGION :",ai )
+    document.writeln("RESIDENTIAL ADDRESS:",aj )
+    document.writeln("CNIC NO:",ak )
+    document.writeln("CONTACT NO:",al )
+    document.writeln("MARK OF IDENTIFICATION:",am )
+    document.writeln("POST CODE:",an )
+    document.writeln("COMPULSORY SUBJECTS: URDU NORMAL",ao)
+    document.writeln("COMPULSORY SUBJECTS: URDU EASY",ap)
+    document.writeln("COMPULSORY SUBJECTS: URDU NON MOTHER TONGUE",aq)
+    document.writeln("COMPULSORY SUBJECTS: HISTORY & CULTURE OF PAKISTAN",ar)
+    document.writeln("COMPULSORY SUBJECTS: ENGLISH NORMAL",as)
+    document.writeln("COMPULSORY SUBJECTS: ENGLISH ADVANCED",at)
+    document.writeln("COMPULSORY SUBJECTS: PHYSICS ADVANCED",au)
+    document.writeln("COMPULSORY SUBJECTS: CHEMISTRY",av)
+    document.writeln("COMPULSORY SUBJECTS: MATHEMATICS EASY",aw)
+    document.writeln("COMPULSORY SUBJECTS: MATHEMATICS HARD",at)
+    document.write("</pre>")
+}
+    
